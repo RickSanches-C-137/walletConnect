3e1d54cd131a5219d26222f1b59201d9aa53a582
evil-morty137 <137.evilmorty@gmail.com>
2024-07-17 09:14:48 +0100

    render index

--- src/app.ts
+++ src/app.ts
@@ -40,7 +40,7 @@ app.use(
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, 'views'));
 // Serve static files from the public directory
 // app.use(express.static(path.join(__dirname, 'public')));
 
